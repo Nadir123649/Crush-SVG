20c0be1e57ae16a0e6a05e2bc373756cccdde4f4
programmer-Raheem <abdulraheem55jutt@gmail.com>
2026-08-18 12:12:12 +0500

    frontedn pushed

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
           <div className="flex flex-col items-center md:items-start w-auto md:w-[127px] gap-[12px] md:gap-[16px]">
             <h4 className="font-heading font-bold text-[14px] leading-[100%] text-[#353A3E] mb-[4px]">Company</h4>
             <Link href="/about" className="font-body font-normal text-[13px] md:text-[14px] leading-[100%] text-[#4B5563] hover:text-brand-primary transition-colors">About Us</Link>
-            <Link href="/privacy-policy" className="font-body font-normal text-[13px] md:text-[14px] leading-[100%] text-[#4B5563] hover:text-brand-primary transition-colors">Cookies</Link>
+            <Link href="/sessions" className="font-body font-normal text-[13px] md:text-[14px] leading-[100%] text-[#4B5563] hover:text-brand-primary transition-colors">Cookies</Link>
             <Link href="/terms" className="font-body font-normal text-[13px] md:text-[14px] leading-[100%] text-[#4B5563] hover:text-brand-primary transition-colors">Terms of Service</Link>
           </div>
         </div>
