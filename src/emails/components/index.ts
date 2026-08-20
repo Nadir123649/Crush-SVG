4dc9fc9d3c29ec525dbbfea348db61d557501e79
hassan-hereee <hi.03004500@gmail.com>
2026-08-20 12:41:31 +0500

    chore: remove unused react-email TSX stack (email.ts uses static HTML)

--- src/emails/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as EmailLayout } from "./EmailLayout";
-export { default as EmailButton } from "./EmailButton";
-export { default as EmailDivider } from "./EmailDivider";
-export { default as EmailText } from "./EmailText";
--- src/emails/renderEmail.ts
+++
@@ -1,6 +0,0 @@
-import { render } from "@react-email/components";
-import type { ReactElement } from "react";
-
-export async function renderEmail(component: ReactElement): Promise<string> {
-  return render(component);
-}
