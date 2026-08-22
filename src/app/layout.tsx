d4003401c7d6f6b0c3e6060c6efc12c5954cb8ef
Nadir123649 <nadir.novatore@gmail.com>
2026-08-23 02:16:28 +0500

    feat: Add Google AdSense script for monetization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2946217028626519"
+          crossOrigin="anonymous"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(getWebSiteSchema()) }}
